6a4ba6d44f979872531204dfda6a92512f6533e7
liquid-krn <alagoke@gmail.com>
2024-09-23 21:24:27 +0100

    first commit

--- src/signup.js
+++ src/signup.js
@@ -9,11 +9,11 @@ function Signup() {
             <div>
             <form>
                 <div className="container ">
-                <div className="card mt-4">
+                <div className="card mt-5">
                     <Input p="Enter Email Address" n="smail" ty="email" />
                     <Input p="Enter Password" n="s-password"  ty="password"/>
-                    <div className="d-grid gap-2 col-6 mx-auto">
-                    <Button className='btn btn-success grow mt-3 ' t="Signup" ty="submit" />
+                    <div className="d-grid  mx-auto">
+                    <Button className='btn btn-success grow mt-3 cb' t="Signup" ty="submit" />
                     </div>
                   
                 </div>
